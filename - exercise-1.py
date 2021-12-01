cc2833ab8f6032cc922be26631d71417ccd9e5ae
Abdulazizoon <95086547+Abdulazizoon@users.noreply.github.com>
2021-12-01 14:27:33 +0600

    Add files via upload

---
+++ - exercise-1.py
@@ -0,0 +1,20 @@
+x1 = str(int(float(5))) # type - str
+x2 = 'aa' == 'AA'  # type - bool
+x3 = {i: i**2 for i in range(5)} #type - dict
+x4 = set(list((5, 6, 7))) #type - set
+a1 = {1: 1, 2: 4, 3: 9} # type - dict
+x = a.get(4) #type - None Type
+x5 = [].append('j') #type - None type
+for i in range(10): #type - str
+    if i < 5:
+        x = 'hello'
+    else:
+        x = 5
+x6 = input('Enter and integer: ') #type - str
+a = 5 #type int
+b = [1, 3, 5, ] #type - list
+x_1 = 'x' #type str
+r = a, b, x = x, a, b #type - tuple
+def func(x, y=5):
+    return x + y
+x = func('Jaguar ', 'hunter') #type - str
